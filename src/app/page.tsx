97bb71c61d2302ec1803848a92e970ae1d6ac1b8
anshuraghv12 <anshuraghav1204@gmail.com>
2025-05-07 12:01:36 +0530

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,192 +1,65 @@
-// "use client"
-// // import Image from "next/image";
-// // import {Demo} from "../components/customComponents/Hero/IndiaMap"
-// import Hero from "@/components/customComponents/Hero/Hero";
-// import TechStack from "@/components/customComponents/Technology/Technology";
-// import {TopLeaders} from "@/components/customComponents/TopLeaders/TopLeaders";
-// import {LeftRightAlumniCarousel} from "@/components/customComponents/CompaniesCarousel/CompaniesCarousel"; import Projets from "@/components/customComponents/Projects/Project";
-// import {OutCome}  from "@/components/customComponents/OutCome/OutCome";
-// import VideoTestimonials from "@/components/customComponents/Testimonials/VideoTestimonials";
+"use client"
+// import Image from "next/image";
+// import {Demo} from "../components/customComponents/Hero/IndiaMap"
+import Hero from "@/components/customComponents/Hero/Hero";
+import TechStack from "@/components/customComponents/Technology/Technology";
+import {TopLeaders} from "@/components/customComponents/TopLeaders/TopLeaders";
+import {LeftRightAlumniCarousel} from "@/components/customComponents/CompaniesCarousel/CompaniesCarousel"; import Projets from "@/components/customComponents/Projects/Project";
+import {OutCome}  from "@/components/customComponents/OutCome/OutCome";
+import VideoTestimonials from "@/components/customComponents/Testimonials/VideoTestimonials";
 
-// import Awards from "@/components/customComponents/Awards/Awards";
-// import Benifits from "@/components/customComponents/Benefits/Benefits";
-
-
-// import FeatureGrid from "@/components/customComponents/Tedx/Feature"
-// import MediaSection from "@/components/customComponents/Media/Media";
-// import Mentor from "@/components/customComponents/Mentor/Mentor";
-
-// import Gallery from "@/components/customComponents/Gallery/Gallery"
-// import Blogs from "@/components/customComponents/Blogs/Blogs"
-// import ImageTestimonials from "@/components/customComponents/Testimonials/TestimonialsImage";
-// import Query from "@/components/customComponents/Query/Query";
-// import OutComeGallary from "@/components/customComponents/OutCome/OutComeGallary";
-// import JazbaaStartupPlatform from "@/components/customComponents/OutCome/JazbaaStartp/JazbaaStartup";
-
-// import Community from "@/components/customComponents/Community/Community";
-// import { MentorScroll } from "@/components/customComponents/Mentor/mentorScroll";
-// import Map from "@/components/customComponents/Map/Map";
-// import { JazbaaTimeLine } from "@/components/customComponents/JazbaaTimeLine/JazbaaTimeLine";
-
-// import Footer3 from "@/components/Footer3";
-
-// import Hero2 from "@/components/customComponents/Hero/Hero2";
-// import Timeline from "@/components/customComponents/TimeLine/TimeLine";
-// import InvestorCarousel from "@/components/customComponents/InverterVideo/Carousel ";
-// import RecapPreviousYear from "@/components/customComponents/recap-previous-year/RecapPreviousYear";
-// import SupportContact from "@/components/customComponents/Support/Support";
-// // import MobileGallery from "@/components/customComponents/Gallery/MobileGallery";
-// import BentoGallery from "@/components/customComponents/Gallery/Gallery";
-// import Experts from "@/components/customComponents/Mentor/Experts";
-// import Awards2 from "@/components/customComponents/Awards/Awards2";
-// // import Timeline from "@/components/customComponents/levels/LearningPath/TimeLine";
+import Awards from "@/components/customComponents/Awards/Awards";
+import Benifits from "@/components/customComponents/Benefits/Benefits";
 
 
+import FeatureGrid from "@/components/customComponents/Tedx/Feature"
+import MediaSection from "@/components/customComponents/Media/Media";
+import Mentor from "@/components/customComponents/Mentor/Mentor";
 
+import Gallery from "@/components/customComponents/Gallery/Gallery"
+import Blogs from "@/components/customComponents/Blogs/Blogs"
+import ImageTestimonials from "@/components/customComponents/Testimonials/TestimonialsImage";
+import Query from "@/components/customComponents/Query/Query";
+import OutComeGallary from "@/components/customComponents/OutCome/OutComeGallary";
+import JazbaaStartupPlatform from "@/components/customComponents/OutCome/JazbaaStartp/JazbaaStartup";
 
-// // export default function Home() {
-// //   return (
-// //     <div >
-// // {/* <Hero></Hero> */}
+import Community from "@/components/customComponents/Community/Community";
+import { MentorScroll } from "@/components/customComponents/Mentor/mentorScroll";
+import Map from "@/components/customComponents/Map/Map";
+import { JazbaaTimeLine } from "@/components/customComponents/JazbaaTimeLine/JazbaaTimeLine";
 
-// // <Hero2></Hero2>
-// // <TechStack/>
-// // <RecapPreviousYear />
+import Footer3 from "@/components/Footer3";
 
-// // <Mentor/>
-// // <Experts/>
-// // <MentorScroll/>
-// // <Awards/>
-// // <TopLeaders/>
-// // <Community/>
-// // <Projets/>
-// // <Map/>
-// // {/* <Gallery/> */}
-// // <BentoGallery/>
-// // <Benifits/>
-// // <Timeline/>
-// // <OutCome/>
-// // <JazbaaStartupPlatform/>
-
-// // <OutComeGallary/>
-// // <InvestorCarousel/>
-// // <JazbaaTimeLine/>
-// // <LeftRightAlumniCarousel/>
-// // <VideoTestimonials/>
-
-// // <ImageTestimonials/>
-// // <FeatureGrid/>
-// // <Query/>
-// // <MediaSection/>
-
-// // {/* <Timeline /> */}
+import Hero2 from "@/components/customComponents/Hero/Hero2";
+import Timeline from "@/components/customComponents/TimeLine/TimeLine";
+import InvestorCarousel from "@/components/customComponents/InverterVideo/Carousel ";
+import RecapPreviousYear from "@/components/customComponents/recap-previous-year/RecapPreviousYear";
+import SupportContact from "@/components/customComponents/Support/Support";
+// import MobileGallery from "@/components/customComponents/Gallery/MobileGallery";
+import BentoGallery from "@/components/customComponents/Gallery/Gallery";
+import Experts from "@/components/customComponents/Mentor/Experts";
+import Awards2 from "@/components/customComponents/Awards/Awards2";
+// import Timeline from "@/components/customComponents/levels/LearningPath/TimeLine";
 
 
 
-
-
-
-
-
-
-
-// // <div>
-
-
-
-// // {/* <InitPlacement/> */}
-// // </div>
-// // {/* <FactAndFiqure/> */}
-
-// // {/* <Blogs/> */}
-
-// // {/* <Tedx/> */}
-// // {/* <Tedx2></Tedx2> */}
-// // {/* FeatureGrid is TedX */}
-
-
-
-
-
-
-// // {/* <Mentor2/> */}
-
-
-
-// // <SupportContact/>
-// //     </div>
-// //   );
-// // }
-
-
-
-
-
-// import { useEffect, useState } from 'react';
-// import QueryModal from "@/components/customComponents/QueryModal";
-// // ... your existing imports
 
 // export default function Home() {
-//   const [showModal, setShowModal] = useState(false);
-
-//   useEffect(() => {
-//     // Show modal after 10 seconds
-//     const timer = setTimeout(() => {
-//       setShowModal(true);
-//     }, 10000);
-
-//     // Cleanup timer
-//     return () => clearTimeout(timer);
-//   }, []);
-
 //   return (
-//     <div>
-//       <QueryModal 
-//         isOpen={showModal} 
-//         onClose={() => setShowModal(false)} 
-//       />
+//     <div >
+// {/* <Hero></Hero> */}
 
-//       {/* Your existing components */}
-//       <Hero2 />
-//       <TechStack/>
-//       <RecapPreviousYear />
-//       <Mentor/>
-//       <Experts/>
-//       <MentorScroll/>
-//       <Awards/>
-//       <TopLeaders/>
-//       <Community/>
-//       <Projets/>
-//       <Map/>
-//       <BentoGallery/>
-//       <Benifits/>
-//       <Timeline/>
-//       <OutCome/>
-//       <JazbaaStartupPlatform/>
-//       <OutComeGallary/>
-//       <InvestorCarousel/>
-//       <JazbaaTimeLine/>
-//       <LeftRightAlumniCarousel/>
-//       <VideoTestimonials/>
-//       <ImageTestimonials/>
-//       <FeatureGrid/>
-//       <Query/>
-//       <MediaSection/>
-//       <SupportContact/>
 // <Hero2></Hero2>
 // <TechStack/>
 // <RecapPreviousYear />
 
 // <Mentor/>
-
-// <MentorScroll/>
 // <Experts/>
+// <MentorScroll/>
 // <Awards/>
-
 // <TopLeaders/>
 // <Community/>
 // <Projets/>
-// <Awards2/>
 // <Map/>
 // {/* <Gallery/> */}
 // <BentoGallery/>
@@ -249,40 +122,9 @@
 
 
 
-"use client"
 import { useEffect, useState } from 'react';
-import Hero from "@/components/customComponents/Hero/Hero";
-import TechStack from "@/components/customComponents/Technology/Technology";
-import {TopLeaders} from "@/components/customComponents/TopLeaders/TopLeaders";
-import {LeftRightAlumniCarousel} from "@/components/customComponents/CompaniesCarousel/CompaniesCarousel"; 
-import Projets from "@/components/customComponents/Projects/Project";
-import {OutCome} from "@/components/customComponents/OutCome/OutCome";
-import VideoTestimonials from "@/components/customComponents/Testimonials/VideoTestimonials";
-import Awards from "@/components/customComponents/Awards/Awards";
-import Benifits from "@/components/customComponents/Benefits/Benefits";
-import FeatureGrid from "@/components/customComponents/Tedx/Feature";
-import MediaSection from "@/components/customComponents/Media/Media";
-import Mentor from "@/components/customComponents/Mentor/Mentor";
-import Gallery from "@/components/customComponents/Gallery/Gallery";
-import Blogs from "@/components/customComponents/Blogs/Blogs";
-import ImageTestimonials from "@/components/customComponents/Testimonials/TestimonialsImage";
-import Query from "@/components/customComponents/Query/Query";
-import OutComeGallary from "@/components/customComponents/OutCome/OutComeGallary";
-import JazbaaStartupPlatform from "@/components/customComponents/OutCome/JazbaaStartp/JazbaaStartup";
-import Community from "@/components/customComponents/Community/Community";
-import { MentorScroll } from "@/components/customComponents/Mentor/mentorScroll";
-import Map from "@/components/customComponents/Map/Map";
-import { JazbaaTimeLine } from "@/components/customComponents/JazbaaTimeLine/JazbaaTimeLine";
-import Footer3 from "@/components/Footer3";
-import Hero2 from "@/components/customComponents/Hero/Hero2";
-import Timeline from "@/components/customComponents/TimeLine/TimeLine";
-import InvestorCarousel from "@/components/customComponents/InverterVideo/Carousel ";
-import RecapPreviousYear from "@/components/customComponents/recap-previous-year/RecapPreviousYear";
-import SupportContact from "@/components/customComponents/Support/Support";
-import BentoGallery from "@/components/customComponents/Gallery/Gallery";
-import Experts from "@/components/customComponents/Mentor/Experts";
-import Awards2 from "@/components/customComponents/Awards/Awards2";
 import QueryModal from "@/components/customComponents/QueryModal";
+// ... your existing imports
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
@@ -303,34 +145,76 @@ export default function Home() {
         isOpen={showModal} 
         onClose={() => setShowModal(false)} 
       />
+      
 
-      <Hero2 />
-      <TechStack />
-      <RecapPreviousYear />
-      <Mentor />
-      <MentorScroll />
-      <Experts />
-      <Awards />
-      <Awards2 />
-      <TopLeaders />
-      <Community />
-      <Projets />
-      <Map />
-      <BentoGallery />
-      <Benifits />
-      <Timeline />
-      <OutCome />
-      <JazbaaStartupPlatform />
-      <OutComeGallary />
-      <InvestorCarousel />
-      <JazbaaTimeLine />
-      <LeftRightAlumniCarousel />
-      <VideoTestimonials />
-      <ImageTestimonials />
-      <FeatureGrid />
-      <Query />
-      <MediaSection />
-      <SupportContact />
+<Hero2></Hero2>
+<TechStack/>
+<RecapPreviousYear />
+
+<Mentor/>
+
+<MentorScroll/>
+<Experts/>
+<Awards/>
+
+<TopLeaders/>
+<Community/>
+<Projets/>
+<Awards2/>
+<Map/>
+{/* <Gallery/> */}
+<BentoGallery/>
+<Benifits/>
+<Timeline/>
+<OutCome/>
+<JazbaaStartupPlatform/>
+
+<OutComeGallary/>
+<InvestorCarousel/>
+<JazbaaTimeLine/>
+<LeftRightAlumniCarousel/>
+<VideoTestimonials/>
+
+<ImageTestimonials/>
+<FeatureGrid/>
+<Query/>
+<MediaSection/>
+
+{/* <Timeline /> */}
+
+
+
+
+
+
+
+
+
+
+<div>
+
+
+
+{/* <InitPlacement/> */}
+</div>
+{/* <FactAndFiqure/> */}
+
+{/* <Blogs/> */}
+
+{/* <Tedx/> */}
+{/* <Tedx2></Tedx2> */}
+{/* FeatureGrid is TedX */}
+
+
+
+
+
+
+{/* <Mentor2/> */}
+
+
+
+<SupportContact/>
     </div>
   );
 }
